d45cd48307a0a699b4cb4ed89752ff2c597cd75d
Christopher Brown <christopher.r.brown@aggiemail.usu.edu>
2016-12-01 17:50:03 -0700

    Finished Reform_Tags
    Now converts all files in Image_Tags to properly contain sparse lists that can be used to train the SVC.

--- Reform_Tags.py
+++ Reform_Tags.py
@@ -21,19 +21,17 @@ tag_dictionary = {}
 pat = r'\], \['
 for path, dir_list, file_list in os.walk(IMG_DIR + '/Image_Tags/'):
     for tag_file in file_list:
-        tag_file = open(IMG_DIR + '/Image_Tags/' + tag_file)
-        tags = tag_file.read()
-        tags = json.loads(tags)
+        tag_file = open(IMG_DIR + '/Image_Tags/' + tag_file, 'r')
+        tags_list = tag_file.read()
+        tags_list = json.loads(tags_list)
         tag_file.close()
 
-        for tag in tags:
-            for item in tag:
-                tag_dictionary[item] = 0
+        for tags in tags_list:
+            for tag in tags:
+                tag_dictionary[tag] = 0
 
 open('Image_Tags.txt', 'w').close()
 f = open('Image_Tags.txt', 'a')
-f.write(str(len(tag_dictionary)))
-f.write('\n')
 f.write(json.dumps(tag_dictionary))
 f.close()
 
@@ -41,3 +39,23 @@ f.close()
 # Goes through all the files in 'Image_Tags' and changes
 # the JSON objects so they are 1's and 0's and correspond
 # to the large tag collection created prior.
+total_tags_array = [key for key in tag_dictionary]
+for path, dir_list, file_list in os.walk(IMG_DIR + '/Image_Tags/'):
+    for tag_file in file_list:
+        f = open(IMG_DIR + '/Image_Tags/' + tag_file, 'r')
+        tags_list = f.read()
+        tags_list = json.loads(tags_list)
+        f.close()
+        f = open(IMG_DIR + '/Image_Tags/' + tag_file, 'w')
+        
+        for tags in tags_list:
+            copy_array = list(total_tags_array)
+            for n, i in enumerate(copy_array):
+                if i in tags:
+                    copy_array[n] = 1
+                else:
+                    copy_array[n] = 0
+            f.write(json.dumps(copy_array))
+            f.write('\n')
+        f.close()
+        
