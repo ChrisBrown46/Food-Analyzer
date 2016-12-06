2933cbddb775c7736019fa9eef05a14de5603f5d
Lyle Olsen <komeaolsen@gmail.com>
2016-12-05 17:37:22 -0700

    Worked on cleaning up the code and got ready for training.

--- train2.py
+++ train2.py
@@ -1,16 +1,8 @@
-import sys
-import json
-from clarifai import rest
 from clarifai.rest import ClarifaiApp
 import os, os.path
 import numpy as np
 from sklearn import svm
-from collections import defaultdict
 from sklearn.externals import joblib
-from sklearn import preprocessing
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-import ast
 
 with open('config') as f:
     credentials = f.read().splitlines()
@@ -21,61 +13,48 @@ model = app.models.get("food-items-v1.0")
 
 clf = svm.SVC(kernel='linear')
 
-def train():
 
+def train():
+    targetList = []
+
+    # creating the list for the target values. In our case it's the number of food label we have.
+    for i in xrange(97):
+        targetList.append(i)
 
     # reading in the images for training
-    Path = "Image_Tags/Tag_apple_pie.txt"
-    Path2 = "Image_Tags/Tag_baby_back_ribs.txt"
-    labelPath = "images2"
+    tagsPath = "Image_Tags"
 
-    testTarget = []
-    foodList = []
+    # reading the vectorized tags and training the SVM with them.
+    # The fits the SVM line by line. We may decide to change this based on the results we get.
+    for item in os.listdir(tagsPath):
+        f = open(tagsPath + "/" + item, 'r')
+        for line in f:
+            clf.fit(line, targetList)
 
-
-    labelNum = 1
-
-    for item in os.listdir(labelPath):
-        for image in os.listdir(labelPath + "/" + item):
-
-            output = model.predict_by_filename(filename=labelPath + "/" + item + "/" + image)
-            output = output[u'outputs'][0][u'data'][u'concepts']
-
-            for food in output:
-                foodList.append(food[u'name'])
-
-            count_vect = CountVectorizer()
-            training = count_vect.fit_transform(foodList)
-
-            print training.shape
-            for count in xrange(training.shape[0]):
-                testTarget.append(count)
-
-            clf.fit(training, testTarget)
-            joblib.dump(clf, 'test.pkl')
-
-            del foodList[:]
-            del testTarget[:]
+    # saving the training for later use.
+    joblib.dump(clf, 'test.pkl')
 
 
+#We'll need to change this fucntion so that we use the reform tags method of vectorizing
 def test():
     clf2 = joblib.load('test.pkl')
 
     testList = []
+
     testOutput = model.predict_by_filename('applePieTest.jpg')
     testOutput = testOutput[u'outputs'][0][u'data'][u'concepts']
+
     for testFood in testOutput:
         testList.append(testFood['name'])
 
-    testVector = CountVectorizer()
-    test1 = testVector.fit_transform(testList)
+    # to do - vecortize the image tags
 
-    print test1.shape
-    prediction = clf2.predict(test1)
+    # print test1.shape
+    # prediction = clf2.predict(test1)
 
-    print prediction
-
+    #print prediction
 
 
 
-test()
+
+train()
