7a6575d0c1544721cfd8f41cce08d6e7c9d8ed9a
Christopher Brown <christopher.r.brown@aggiemail.usu.edu>
2016-12-08 17:30:32 -0700

    Created SVC
    Create_SVC creates a new SVC based on Image_Tags and the Image_Tags folder. The SVC is then stored in training.pkl
    Analyze_Food's main function takes a file and returns what food the SVC thinks it is.

---
+++ Analyze_Food.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import os, os.path
+import json
+import numpy as np
+from sklearn import svm
+from sklearn.externals import joblib
+from Create_Training_Data import generate_tags_for_image
+
+
+def classify_file(filepath):
+    # First generate tags for the image, then open our SVC
+    # and super tag list up
+    output = generate_tags_for_image(filepath)
+    svc = joblib.load('training.pkl')
+    tags = json.loads(open('Image_Tags.txt', 'r').read())
+
+    # Turn our recieved tags into a sparse array that is
+    # based off of our super tag list
+    sparse_array = []
+    for tag in tags:
+        if tag in output:
+            sparse_array.append(1)
+        else:
+            sparse_array.append(0)
+    sparse_array = np.array(sparse_array)
+
+    return svc.predict(sparse_array)[0]
---
+++ Create_SVC.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import os, os.path
+import json
+from sklearn import svm
+import numpy as np
+from sklearn.externals import joblib
+
+
+# Variable must be changed to match the directory of image tags.
+IMG_DIR = 'D:/Personal/School/Third Year/CS 5600/Food Analyzer'
+
+
+# Create the linear SVC
+svc = svm.SVC(kernel = 'linear')
+
+
+# Retrieve the training data and store it as X and Y
+X = []
+Y = []
+for path, dir_list, file_list in os.walk(IMG_DIR + '/Image_Tags/'):
+    for tag_file in file_list:
+        for x in xrange(49):
+            Y.append(tag_file[4:-4])
+            
+        lines = [json.loads(line) for line in
+                 open(IMG_DIR + '/Image_Tags/' + tag_file, 'r')]
+        
+        for line in lines:
+            X.append(line)   
+X = np.array(X)
+
+
+# Train the SVC
+svc.fit(X, Y)
+
+
+# Save the training in a pickle file
+joblib.dump(svc, 'training.pkl')
+
